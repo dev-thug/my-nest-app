f914b8f54cddd9cd174662681fe9b4288b20a739
dev-thug <de0978@gmail.com>
2023-02-26 23:03:15 +0900

    Add config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,7 +1,9 @@
 import { Auth } from './../auth/entities/auth.entity';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Board } from 'src/boards/board.entity';
+import * as config from 'config';
 
+const dbConfig = config.get('db');
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
