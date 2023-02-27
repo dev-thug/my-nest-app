77e6960959c1130cc7c4b7d015cac4e040a221dd
dev-thug <de0978@gmail.com>
2023-02-28 06:41:29 +0900

    feat: Add todo resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,18 @@ import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [BoardsModule, TypeOrmModule.forRoot(typeORMConfig), AuthModule],
+  imports: [
+    BoardsModule,
+    TypeOrmModule.forRoot(typeORMConfig),
+    AuthModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://de0978:QxaWdAax9eGLDE5J@cluster0.sablhsh.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    TodoModule,
+  ],
 })
 export class AppModule {}
